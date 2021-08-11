63ea5b5fb987e31a31125610d15034b5413b4105
growywl <monkeykrit20@gmail.com>
2021-08-11 13:49:23 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text } from 'react-native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { auth } from './firebase'; 
+import LoginScreen from './screens/sessions/LoginScreen';
 
+const Stack = createStackNavigator();
 export default function App() {
+  const [signedIn, setsignedIn] = useState(false);
+  
+  auth.onAuthStateChanged((use) => {
+    if (user) {
+      setsignedIn(true);
+    } else {
+      setsignedIn(false);
+    }
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer theme={DefaultTheme}>
+      {signedIn
+        ? (
+          <Text>Signed In</Text>
+        ) : (
+          <>
+          <StatusBar style="light" />
+          <Stack.Navigator
+          mode="card"
+          screenOptions={{}}
+          >
+            <Stack.Screen
+            name="signIn"
+            component={LoginScreen}
+            options={{
+              title: 'Sign In',
+              headerStyle: {
+                backgroundColor: '#29434e',
+                borderBottomColor: '#29434e',
+              },
+              headerTintColor: '#fff'
+            }}/>
+            </Stack.Navigator>
+            </>
+        )
+      }
+      </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
