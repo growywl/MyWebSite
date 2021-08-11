76f7fb6e5f4494f36b73d56625a60a67cc43a578
growywl <monkeykrit20@gmail.com>
2021-08-11 13:49:27 +0700

    Create LoginScreen.js

---
+++ screens/sessions/LoginScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+const LoginScreen = () => {
+    return (
+        <View>
+            <Text>Login Screen</Text>
+            <h1>
+            <Text>Hello Krit</Text>
+            </h1>
+            <h3>
+            <Text>11/8/2021</Text>
+            </h3>
+        </View>
+    )
+};
+export default LoginScreen;
